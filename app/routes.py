87597d8057f2a7ff46a1703ec87b025317c6dda0
Phospor <devbhaven@outlook.com>
2025-02-15 15:50:26 -0700

    question template commit

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,14 @@ from flask import render_template, request, redirect, url_for
 @app.route('/index')
 def index():
     return 'hello world'
+questions = ['question: ', 'question: ', 'question: ', 'question: ', 'question: ']
 
 @app.route('/form')
 def form():
     return render_template('form.html')
+
+def read_form():
+    data = request.form
+    return {
+        ''
+    }
